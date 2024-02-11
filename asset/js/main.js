52e912ae9bd52799375ce75bd748f08bb4252697
Mohammad-Aljada <mohammadaljad.1234@gmail.com>
2024-02-11 14:40:56 +0200

    add delete to task

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,22 +4,22 @@ let tasks = [];
 function addTask(e) {
   e.preventDefault();
   let task = {};
-
   task.value = document.querySelector(".formTask .input").value;
   task.isChecked = false;
   tasks.push(task);
+  setdataFromLocalstorage();
   if (!task) return alert("Please enter a task");
-
-  printData(e);
+  e.target.reset();
+  printData();
 }
-
-function printData(e) {
+function printData() {
   let result = "";
-
-  result += tasks
+  let data = getdataFromLocalStorage();
+  result += data
     .map(function (task) {
       return `
-        <li class="task">
+      <div class="task-group">
+          <li class="task">
         <input type="checkbox"  name="checkbox" class="checkbox" ${
           task.isChecked ? "checked" : ""
         }/>
@@ -27,16 +27,18 @@ function printData(e) {
           task.isChecked ? "line" : ""
         }">${task.value}</span>
         </li>
-        
+        <button class="btn-deleteTask">Delete</button>
+      </div>
+    
         `;
     })
     .join(" ");
-
   document.querySelector(".TasksList").innerHTML = result;
-  e.target.reset();
   addline();
+  deleteTask();
 }
 function addline() {
+  let getdata = getdataFromLocalStorage();
   const li = document.querySelectorAll(".task");
   const check = document.querySelectorAll('.task input[type="checkbox"]');
   for (let i = 0; i < check.length; i++) {
@@ -54,3 +56,24 @@ function addline() {
     };
   }
 }
+function getdataFromLocalStorage() {
+  let getdata = localStorage.getItem("tasks");
+  let data = JSON.parse(getdata);
+  return data;
+}
+function setdataFromLocalstorage() {
+  let setdata = JSON.stringify(tasks);
+  localStorage.setItem("tasks", setdata);
+}
+function deleteTask() {
+  let getdata = getdataFromLocalStorage();
+  const btn = document.querySelectorAll(".task-group .btn-deleteTask");
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].onclick = function (e) {
+      btn[i].closest(".task-group").remove();
+      tasks.splice(i, 1);
+      setdataFromLocalstorage();
+    };
+  }
+}
+printData();
